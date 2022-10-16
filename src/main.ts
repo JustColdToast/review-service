c3e43b43c75736dc3774147f24cf083b6f3e4d28
Kanwarpal Brar <kanwar@cloudsparklabs.com>
2022-10-16 05:31:07 -0400

    - I did stuff

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Set up dynamoose region
+  const dynamoose = require('dynamoose');
+  dynamoose.aws.ddb.set(new dynamoose.aws.ddb.DynamoDB({'region': 'us-east-1'}));
+  
   await app.listen(3000);
 }
 bootstrap();
